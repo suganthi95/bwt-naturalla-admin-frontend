34e93bcaa9d6713ee126f7d92b1d993fb564f52f
bavabahurdeen <bahurdeen.works@blackwinstech.com>
2024-12-13 22:59:21 +0530

    new changes

--- src/components/layout/UpgradeToProDialog.tsx
+++ src/components/layout/UpgradeToProDialog.tsx
@@ -114,14 +114,14 @@ function UpgradeToProDialog({ planName, planEndDate, clickEvent }: Props) {
             >
               Upgrade Now
             </Button>
-            {planName == "free trial" &&
+            {planName == "free trial" && !balanceDays &&(
               <Button
                 onClick={() => setModal(false)}
                 className="p-2 px-8"
               >
                 Maybe Later
               </Button>
-            }
+            )}
           </div>
         </AlertDialogFooter>
       </AlertDialogContent>
