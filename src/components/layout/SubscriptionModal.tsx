ca7240ca96d86341a941ffbf4d6f5bd2d5a6eb48
bavabahurdeen <bahurdeen.works@blackwinstech.com>
2024-12-16 16:58:31 +0530

    dark mode ui changes

--- src/components/layout/SubscriptionModal.tsx
+++ src/components/layout/SubscriptionModal.tsx
@@ -167,7 +167,7 @@ function SubscriptionModal({ triggerPaymentDialog, setTriggerPaymentDialog }: { 
                 </div>
 
                 <p className="text-center text-xs md:text-sm text-slate-500">Experience the full capabilities of IntelliResponse without any commitment.</p>
-                <div className="text-center flex flex-row items-center gap-2 font-medium mx-auto">
+                <div className="text-center flex flex-row items-center gap-2 font-medium mx-auto dark:text-white">
                     <p>Monthly</p>
                     <Switch
                         checked={planType === "yearly"}
@@ -250,7 +250,7 @@ function SubscriptionModal({ triggerPaymentDialog, setTriggerPaymentDialog }: { 
                         </div>
 
                         <div className="mt-2">
-                            <Button disabled={isPending} onClick={() => buyNowOnclick(proPlanData.plan_id, proPlanData.country)} size="lg" className="w-full bg-primary hover:bg-primary/80">Buy Now</Button>
+                            <Button disabled={isPending} onClick={() => buyNowOnclick(proPlanData.plan_id, proPlanData.country)} size="lg" className="w-full bg-primary hover:bg-primary/80 dark:bg-slate-950 dark:text-white">Buy Now</Button>
                         </div>
                     </div>
 
@@ -306,7 +306,7 @@ function SubscriptionModal({ triggerPaymentDialog, setTriggerPaymentDialog }: { 
             setOpenSubscriptionModal(prev => !prev);
             setTriggerPaymentDialog(false)
         }}>
-        <DialogContent className="h-full w-10/12 md:max-w-7xl">
+        <DialogContent className="h-full w-10/12 md:max-w-7xl dark:text-white">
             {plans}
         </DialogContent>
     </Dialog>
